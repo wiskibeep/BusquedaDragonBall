44ddb004fcb667bc7729bea364acf9ae4482745e
wiski <wiski2311@gmail.com>
2026-02-02 18:11:54 +0100

    Antes de modificar las las Trasnsformaciones

--- BusquedaDragonBall/ViewController/DetallePJViewController.swift
+++ BusquedaDragonBall/ViewController/DetallePJViewController.swift
@@ -1,28 +1,33 @@
 import UIKit
 
 class DetallePJViewController: UIViewController, UICollectionViewDataSource {
-
     @IBOutlet weak var imagenPJ: UIImageView!
     @IBOutlet weak var descriptionPJ: UILabel!
-   // @IBOutlet weak var nombrePJ: UILabel!
-
+    // @IBOutlet weak var nombrePJ: UILabel!
     @IBOutlet weak var TransformacionPJ: UICollectionView!
-
-    
-    
     @IBOutlet weak var poderPJ: UILabel!
     @IBOutlet weak var MaxPoderPJ: UILabel!
     @IBOutlet weak var RacePj: UILabel!
     @IBOutlet weak var GenderPJ: UILabel!
     
     
+    
+    @IBOutlet weak var favoriteButtonItem: UIBarButtonItem!
+    
     var personaje: Personaje.Item!
+
+    // Computed property para saber si es favorito
+    var isFavorite: Bool {
+        let favoriteID = UserDefaults.standard.integer(forKey: "favoriteCharacterID")
+        return favoriteID == personaje.id
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.title = personaje.name
         TransformacionPJ.dataSource = self
         cargarDatos()
+        setFavoriteIcon()
     }
 
     func cargarDatos() {
@@ -54,4 +59,46 @@ class DetallePJViewController: UIViewController, UICollectionViewDataSource {
         }
         return cell
     }
+
+    // MARK: - Favoritos
+
+
+
+    @IBAction func SetFavorite(_ sender: Any) {
+        if isFavorite {
+            UserDefaults.standard.removeObject(forKey: "favoriteCharacterID")
+        } else {
+            UserDefaults.standard.set(personaje.id, forKey: "favoriteCharacterID")
+        }
+        setFavoriteIcon()
+    }
+    
+    func setFavoriteIcon() {
+        if isFavorite {
+            favoriteButtonItem.image = UIImage(systemName: "heart.fill")
+        } else {
+            favoriteButtonItem.image = UIImage(systemName: "heart")
+        }
+    }
+    
+    
+    
+    
+    //MARK: compartir
+    
+    var predicion: String? = ""
+    
+    @IBAction func share(_ sender: Any) {
+        if let predicion = predicion {
+            
+            
+            let text = "MI personaje Favoito es \(personaje.name): \(predicion) "
+            let textToShare = [text]
+            let activityViewController = UIActivityViewController(activityItems: textToShare, applicationActivities: nil)
+            activityViewController.popoverPresentationController?.sourceView = self.view
+            self.present(activityViewController, animated: true, completion: nil)
+        }
+    }
+    
+    
 }
