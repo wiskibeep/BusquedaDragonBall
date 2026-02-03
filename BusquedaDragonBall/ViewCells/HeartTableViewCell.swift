b80db791b8981e56fd2f91b74d563510a7fe2fa2
wiski <wiski2311@gmail.com>
2026-02-03 18:29:33 +0100

    Diseñoo y pulido

---
+++ BusquedaDragonBall/ViewCells/HeartTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  HeartTableViewCell.swift
+//  BusquedaDragonBall
+//
+//  Created by Tardes on 3/2/26.
+//
+
+import UIKit
+
+class HeartTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ BusquedaDragonBall/ViewController/HeartViewController.swift
@@ -0,0 +1,29 @@
+//
+//  HeartViewController.swift
+//  BusquedaDragonBall
+//
+//  Created by Tardes on 3/2/26.
+//
+
+import UIKit
+
+class HeartViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
